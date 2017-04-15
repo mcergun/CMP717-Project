6c0e69f19ab3d6ce8f22abdae8c6e49deb29594d
Mert Can Ergün <mert.ergun@mikro-tasarim.com.tr>
2017-04-15 16:45:56 +0300

    corrected parameters according to lipton and pot image

--- code/sd_denoising.m
+++ code/sd_denoising.m
@@ -17,8 +17,8 @@ g = im2double(rgb2gray(imread(fullfile(img_dir, static_img_name)))); %static gui
 %scale-space filtering using joint static and dynamic guidance 
 nei = 0;                   % 0: 4-neighbor 1: 8-neighbor
 lambda = 15;   % regularization parameter
-mu = 60;                  % bw for static guidance
-nu = 30;                 % bw for dymadic guidance
+mu = 600;                  % bw for static guidance
+nu = 300;                 % bw for dymadic guidance
 step = 5;                % num of steps
 issparse = false;
 
