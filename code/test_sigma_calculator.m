b6c8b5f338ea2db77bb871d3c9e411537ef97510
Mert Can Ergün <mcergun@windowslive.com>
2017-04-16 12:57:48 +0300

    added a new noise estimation method

--- code/test_sigma_calculator.m
+++ code/test_sigma_calculator.m
@@ -1,7 +1,9 @@
 folder_path = '../data/sigma';
 base_file = 'lena.jpg';
-noise_sigma = 20;
-image_count = 100;
+noise_sigma = 30;
+image_count = 10;
+
+addpath 'noiselevel'
 
 generate_noisy_image(folder_path, base_file, noise_sigma, image_count);
 
@@ -12,18 +14,23 @@ imsize = size(imread(filenames{1}));
 mn = double(zeros(imsize));
 var = double(zeros(imsize));
 
+sigma2 = 0.0;
+
 for i=1:file_count
     cur_img = double(imread(filenames{i}));
     [mn, var] = estimate_var(mn, var, cur_img, i);
+    sigma2 = sigma2 + (NoiseLevel(cur_img) - sigma2) / i;
 end
 
 sigma = sqrt(var);
 
 mean_sigma = mean2(sigma);
 mean_mn = mean2(mn);
-error = abs((mean_sigma - noise_sigma) / noise_sigma);
+error1 = 100 * abs((mean_sigma - noise_sigma) / noise_sigma);
+error2 = 100 * abs((sigma2 - noise_sigma) / noise_sigma);
 
-fprintf('Estimated sigma value is %f, while original value is %f\n', ...
-    mean_sigma, noise_sigma);
-fprintf('Error margin is %.2f%%\n', error * 100);
-
+fprintf('Original sigma value = %0.2f\n', noise_sigma);
+fprintf('Estimated sigma with 1st method = %0.2f, Error Rate = %0.2f\n', ...
+    mean_sigma, error1);
+fprintf('Estimated sigma with 2nd method = %0.2f, Error Rate = %0.2f\n', ...
+    sigma2, error2);
