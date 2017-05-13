a76356276f6ba66b019d907b5d9a55355b650652
Mert Can Ergün <mcergun@windowslive.com>
2017-05-13 11:50:51 +0300

    moved file io related functions under code/fileio

---
+++ code/fileio/read_file_list.m
@@ -0,0 +1,22 @@
+function [ filelist, listsize ] = read_file_list( folder_path )
+    filelist = fullfile(folder_path, 'filelist.txt');
+    fprintf('Reading file list from "%s"\n', filelist);
+    
+    fid = fopen(filelist);
+    
+    filelist = cell(1,10000);
+    i = 0;
+    
+    tline = fgetl(fid);
+    while ischar(tline)
+        i = i + 1;
+        im_path = fullfile(folder_path, tline);
+        filelist(i) = {im_path};
+        tline = fgetl(fid);
+    end
+    fclose(fid);
+    
+    filelist = filelist(1:i);
+    listsize = i;
+end
+
--- code/mkdir_w_check.m
+++
@@ -1,8 +0,0 @@
-function [ result ] = mkdir_w_check( folder_path )
-    if ~exist(folder_path, 'dir')
-        result = mkdir(folder_path);
-    else
-        result = 1;
-    end
-end
-
