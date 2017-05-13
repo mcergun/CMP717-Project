533ccad7fec6a4c0790dceede0447eca1281378d
Mert Can Ergün <mcergun@windowslive.com>
2017-05-13 20:19:23 +0300

    noisy image generator for TRI_A1 images

--- code/generate_noisy_image.m
+++ code/generate_noisy_image.m
@@ -1,5 +1,6 @@
 function [ im_noisy ] = generate_noisy_image( im, sigma )
-im_noisy = im + randn(size(im)) * sigma;
+noise_data = uint8(randn(size(im)) .* sigma);
+im_noisy = im + noise_data;
 im_noisy(im_noisy < 0) = 0;
 im_noisy(im_noisy > 255) = 255;
 end
---
+++ code/generate_noisy_image_folder.m
@@ -0,0 +1,14 @@
+function [ im_noisy ] = generate_noisy_image_folder( folder_path, sigma )
+
+[img_list, image_count] = read_file_list(folder_path);
+
+for i=1:image_count
+    file_path = img_list{i};
+    im = imread(file_path);
+    im_noisy = generate_noisy_image(im, sigma);
+    extension = file_path(strlength(file_path)-3:strlength(file_path));
+    new_file_name = strrep(img_list{i}, extension, sprintf('_%d%s', sigma, extension));
+    imwrite(uint8(im_noisy), new_file_name);
+end
+end
+
