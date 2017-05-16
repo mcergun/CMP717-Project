d4ae87e5708ea2a494d5b564601274275680b4a6
Gökhan Yalınız <gokhanyaliniz@gmail.com>
2017-05-16 10:08:36 +0300

    workaround is done to folderpath

--- code/fileio/read_image_list.m
+++ code/fileio/read_image_list.m
@@ -5,7 +5,7 @@ file_names = cell(1,10000);
 listsize = 0;
 for i=1:length(dir_details)
 	file_details = dir_details(i);
-    file_path = fullfile(file_details.folder, file_details.name);
+    file_path = fullfile(folder_path, file_details.name);
     if ~file_details.isdir && is_image_file(file_path)
         listsize = listsize + 1;
         file_paths(listsize) = {file_path};
