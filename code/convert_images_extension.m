25ac81450a0063a776211bffa13df5dbcb20b172
Mert Can Ergün <mcergun@windowslive.com>
2017-05-21 22:18:26 +0300

    renamed and updated script that converts image extensions

---
+++ code/convert_images_extension.m
@@ -0,0 +1,18 @@
+function convert_images_extension(folder_path, new_extension)
+addpath 'fileio'
+
+[ file_paths, listsize, file_names ] = read_image_list(folder_path);
+
+for i=1:listsize
+  cur_name = file_names{i};
+  cur_im = imread(file_paths{i});
+  cur_extension = cur_name(end-3:end);
+  if ~strcmp(cur_extension, new_extension)
+      new_name = strrep(cur_name, cur_extension, new_extension); 
+      disp(new_name);
+      imwrite(cur_im, fullfile(folder_path, new_name));
+      delete(fullfile(folder_path, cur_name));
+  end
+end
+
+end
